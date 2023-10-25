5b0d9b86b5987bae3b35feb9931083a40a1f641d
natasha sokolovskaia <8nataha@gmail.com>
2023-10-26 01:07:12 +0300

    init tower short

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+// module.exports = function towelSort(matrix) {
+//     if (matrix === undefined)
+//         matrix = [];
+//     else {
+//         for (let i = 0; i < matrix.length; i++) {
+//             if (i % 2 === 0) {
+//                 matrix[i] = matrix[i].reverse()
+//             }
+//             else {
+//                 matrix[i] = matrix[i]
+//             }
+//         }
+//     }
+//     return matrix.flatMap(matrix[i])
+// }
+
+
+// module.exports = function towelSort (matrix) {
+//     if (matrix === undefined)
+//     matrix = [];
+//     else {
+//         matrix.reduce((accumulator, currentValue, index) => {
+//             if (index % 2) {
+//                 return  accumulator.concat(currentValue.reverse())
+//             } else {
+//                 return  accumulator.concat(currentValue)
+//             }})
+//         }
+//         }
+
+
+
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined)
+    return [];
+else {
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 > 0) {
+            matrix[i] = matrix[i].reverse();
+        }
+    }
+    return matrix.flatMap((num) => num)
+}
 }
